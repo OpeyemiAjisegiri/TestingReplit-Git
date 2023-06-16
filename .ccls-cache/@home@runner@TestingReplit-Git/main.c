df7a464194a212ac9e0aca65d19f4efadc3c9416
OpeyemiAjisegiri <contact.oajisegiri@gmail.com>
2023-06-16 02:06:11 +0000

    Copied My First C Program

--- .ccls-cache/@home@runner@TestingReplit-Git/main.c
+++ .ccls-cache/@home@runner@TestingReplit-Git/main.c
@@ -1,7 +1,21 @@
+/* Testing a git repositories idea */
+/* Copied from first program in C  */
 #include <stdio.h>
 
 int main(void) {
   printf("Hello World\n");
-  printf("My name is Opeyemi Ajisegiri");
+  printf("This is my first program in C language\n");
+  
+  /*Testing Out Some Ideas*/
+  int testNum = 5;
+  printf("The testing number is: %d\n\n", testNum);
+
+  int numA, numB; 
+  printf("Please enter a number:\t");
+  scanf("%d", &numA);
+  printf("Please enter a number:\t");
+  scanf("%d", &numB);
+  printf("The multiplication of the two numbers you entered is: %d\n",(numA * numB));
+
   return 0;
 }
